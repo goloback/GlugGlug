d58940fdb3b3ce54d2e9e37b6de786b7e4e515b0
goloback <adduma2008@gmail.com>
2024-12-31 13:32:38 +0200

    Create game GlugGlug

---
+++ main.py
@@ -0,0 +1,113 @@
+import random
+import sys
+import pygame
+from resource import main_player, monsters, prize, rope, bullet_system
+
+def destroy_bullets():
+    for bullet in bullet_system_object.bullets_list.copy():
+        if bullet.destroy:
+            if bullet in object_list:
+                object_list.remove(bullet)
+            bullet_system_object.bullets_list.remove(bullet)
+
+def bullet_action():
+    bullet_system_object.create_bullets()
+    for bullet in bullet_system_object.bullets_list:
+        if bullet in object_list:
+            if bullet.destroy:
+                object_list.remove(bullet)
+        else:
+            if not bullet.destroy:
+                object_list.append(bullet)
+
+def all_actions(object_list):
+    bullet_action()
+    destroy_bullets()
+    for object in object_list:
+        object.draw()
+        object.move()
+        if str(type(object)) =="<class 'resource.bullet'>":
+            for f in fish_list.copy():
+                if pygame.sprite.collide_rect(f, object):
+                    object.destroy = True
+                    if f in object_list:
+                        object_list.remove(f)
+                        if f in fish_list:
+                            fish_list.remove(f)
+    for i in prize_list:
+        if objects_touch(glug, i) == True and glug.hand == True and i.show == True:
+            i.free = False
+            glug.hand = False
+
+def objects_touch(object1, object2):
+    try:
+        if pygame.sprite.collide_mask(object1, object2):
+            return True
+        else:
+            return False
+    except:
+        return False
+
+pygame.init()
+window = pygame.display.set_mode((1200, 700))
+glug = main_player(window, 'left.png', 'right.png')
+rope = rope(window, 'rope.png', 'rope.png', glug)
+bullet_system_object = bullet_system(window, glug)
+amount_fish_1 = 30
+fish_list = []
+
+for i in range(amount_fish_1):
+    fish = monsters(window, 'left_fish.png', 'right_fish.png')
+    if i % 2 == 0:
+        fish.rect.centerx -= random.randint(0, 10000)
+    else:
+        fish.rect.centerx = random.randint(1200, 11200)
+
+    fish.x = fish.rect.centerx
+    fish_list.append(fish)
+
+prize_list = []
+crest = prize(window, 'crest.png', glug)
+gold = prize(window, 'gold.png', glug)
+cup = prize(window, 'cup.png', glug)
+prize_list.append(crest)
+prize_list.append(gold)
+prize_list.append(cup)
+object_list = [glug]
+object_list += prize_list
+object_list += fish_list
+object_list.append(rope)
+
+while True:
+    print(f'fish {len(fish_list)}')
+    print(f'bullet{len(bullet_system_object.bullets_list)}')
+    window.fill((0, 0, 0))
+    all_actions(object_list)
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            sys.exit()
+        if i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_s or i.key == pygame.K_DOWN:
+                glug.move_down = True
+            if i.key == pygame.K_w or i.key == pygame.K_UP:
+                glug.move_up = True
+            if i.key == pygame.K_a or i.key == pygame.K_LEFT:
+                glug.move_left = True
+            if i.key == pygame.K_d or i.key == pygame.K_RIGHT:
+                glug.move_right = True
+            if i.key == pygame.K_SPACE:
+                bullet_system_object.press_space = True
+
+        if i.type == pygame.KEYUP:
+            if i.key == pygame.K_s or i.key == pygame.K_DOWN:
+                glug.move_down = False
+            if i.key == pygame.K_w or i.key == pygame.K_UP:
+                glug.move_up = False
+            if i.key == pygame.K_a or i.key == pygame.K_LEFT:
+                glug.move_left = False
+            if i.key == pygame.K_d or i.key == pygame.K_RIGHT:
+                glug.move_right = False
+            if i.key == pygame.K_SPACE:
+                bullet_system_object.press_space = False
+
+    pygame.display.flip()
---
+++ resource.py
@@ -0,0 +1,214 @@
+import random
+import time
+
+import pygame
+
+class game_object:
+    def __init__(self, window, imgleft, imgright):
+        self.screen = window
+        try:
+            self.imgleft = pygame.image.load(imgleft)
+            self.imgright = pygame.image.load(imgright)
+            self.image = self.imgleft
+            self.rect = self.image.get_rect()
+        except:
+            self.imgleft = None
+            self.imgright = None
+            self.image = None
+            self.rect = None
+        self.show = True
+
+    def draw(self):
+        if self.show:
+            self.screen.blit(self.image, self.rect)
+
+    def move(self):
+        pass
+
+class bullet(game_object):
+    def __init__(self, window, player):
+        super().__init__(window, None, None)
+        self.player = player
+        self.rect = pygame.Rect(self.player.rect.x - 1, self.player.rect.centery, 10, 2)
+        self.default_x = player.rect.x
+        self.destroy = False
+        if self.player.image == self.player.imgleft:
+            self.direction = 'L'
+        else:
+            self.direction = 'R'
+            self.rect.x += 30
+        self.speed = 0.6
+        self.x = self.rect.x
+
+    def draw(self):
+        if self.show:
+            pygame.draw.rect(self.screen, (248, 248, 0), self.rect)
+
+    def move(self):
+        if self.destroy:
+            return
+        if self.direction == 'L':
+            self.x -= self.speed
+            self.rect.x = self.x
+            if self.x < self.default_x - 450:
+                self.destroy = True
+        else:
+            self.x += self.speed
+            self.rect.x = self.x
+            if self.x > self.default_x + 450:
+                self.destroy = True
+
+class bullet_system:
+    def __init__(self, window, player):
+        self.press_space = False
+        self.last_bullet = None
+        self.bullets_list = []
+        self.screen = window
+        self.player = player
+
+    def create_bullets(self):
+        if self.press_space:
+            if self.last_bullet is None or time.time() - self.last_bullet > 0.5:
+                bullet_ = bullet(self.screen, self.player)
+                self.bullets_list.append(bullet_)
+                self.last_bullet = time.time()
+
+class rope(game_object):
+    def __init__(self, window, imgleft, imgright, player):
+        super().__init__(window, imgleft, imgright)
+        self.player = player
+        self.img_boat = pygame.image.load('boat.png')
+        self.img_boat_rect = self.img_boat.get_rect()
+        self.img_boat_rect.top = 0
+        self.img_blue = pygame.image.load('blue.png')
+        self.img_blue_rect = self.img_blue.get_rect()
+        self.img_blue_rect2 = self.img_blue.get_rect()
+        self.img_blue_rect.top = -20
+        self.img_blue_rect2.top = -20
+        self.img_blue_rect.left = 0
+        self.img_blue_rect2.right = 1200
+        self.img_grass = pygame.image.load('grass.png')
+        self.img_grass_rect = self.img_grass.get_rect()
+        self.img_grass_rect2 = self.img_grass.get_rect()
+        self.img_grass_rect.bottom = 700
+        self.img_grass_rect2.bottom = 700
+        self.img_grass_rect.left = 0
+        self.img_grass_rect2.right = 1200
+
+
+
+    def draw(self):
+        if self.show:
+            if self.player.image == self.player.imgleft:
+                self.rect.centerx = self.player.rect.centerx + 2
+                self.img_boat_rect.centerx = self.player.rect.centerx + 35
+            else:
+                self.rect.centerx = self.player.rect.centerx
+                self.img_boat_rect.centerx = self.player.rect.centerx + 35
+            self.rect.bottom = self.player.rect.top + 4
+            self.screen.blit(self.image, self.rect)
+            self.screen.blit(self.img_blue, self.img_blue_rect)
+            self.screen.blit(self.img_blue, self.img_blue_rect2)
+            self.screen.blit(self.img_boat, self.img_boat_rect)
+            self.screen.blit(self.img_grass, self.img_grass_rect)
+            self.screen.blit(self.img_grass, self.img_grass_rect2)
+
+class main_player(game_object):
+    def __init__(self, window, imgleft, imgright):
+        super().__init__(window, imgleft, imgright)
+        self.rect.centerx = self.screen.get_rect().centerx
+        self.rect.top = 50
+        self.move_down = False
+        self.move_up = False
+        self.move_left = False
+        self.move_right = False
+        self.hand = True
+        self.y = self.rect.centery
+        self.x = self.rect.centerx
+        self.image_move = 0
+
+    def move(self):
+        speed_walk = 0.2
+        speed = 0.5
+        if self.move_down:
+            if self.rect.bottom < 660:
+                self.y += speed
+                self.rect.centery = float(self.y)
+        if self.move_up:
+            if self.rect.top > 50:
+                self.y -= speed
+                self.rect.centery = float(self.y)
+        if self.move_left:
+            self.image = self.imgleft
+            if self.rect.bottom >= 660:
+                self.x -= speed_walk
+                self.walk_animation('Left')
+            else:
+                self.x -= speed
+            self.rect.centerx = self.x
+            if self.rect.right < self.screen.get_rect().left + 5:
+                self.x = self.screen.get_rect().right - 5
+                self.rect.left = self.x
+        if self.move_right:
+            self.image = self.imgright
+            self.x += speed
+            self.rect.centerx = float(self.x)
+            if self.rect.left > self.screen.get_rect().right + 5:
+                self.x = self.screen.get_rect().left + 5
+                self.rect.left = self.x
+
+    def check_touch(self):
+        pass
+    def walk_animation(self, direction):
+        if direction == 'Left':
+            if self.image_move % 5 == 0:
+                if self.image == self.imgleft:
+                    self.image = pygame.image.load('moveL.png')
+                else:
+                    self.image = pygame.image.load('left.png')
+            self.image_move += 1
+
+class monsters(game_object):
+    def __init__(self, window, imgleft, imgright):
+        super().__init__(window, imgleft, imgright)
+        self.rect.centerx = 0
+        self.rect.top = random.randint(60, 630)
+        self.new_point = True
+        self.distance = 0
+        self.x = self.rect.centerx
+        self.y = self.rect.centery
+        self.speed = random.randint(23, 35) / 100
+
+    def move(self):
+        speed = self.speed
+        if self.new_point:
+            self.distance = random.randint(0, 1200)
+            self.new_point = False
+        if self.rect.centerx > self.distance:
+            self.image = self.imgleft
+            self.x -= speed
+            self.rect.centerx = self.x
+            if self.rect.centerx <= self.distance:
+                self.new_point = True
+        else:
+            self.image = self.imgright
+            self.x += speed
+            self.rect.centerx = self.x
+            if self.rect.centerx >= self.distance:
+                self.new_point = True
+
+class prize(game_object):
+    def __init__(self, window, image, glug):
+        super().__init__(window, image, image)
+        self.rect.centerx = random.randint(35, 1165)
+        self.rect.bottom = 660
+        self.free = True
+        self.glug = glug
+
+    def move(self):
+        if self.free == False and self.show == True:
+            self.rect.x = self.glug.rect.x
+            self.rect.y = self.glug.y + 20
+            if self.glug.rect.top < 15:
+                self.show = False
+                self.glug.hand = True
